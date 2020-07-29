587200d365a43ec839c90f4d5fdc5ffc1c909eb9
Katherine <katherinehincapie19@hotmail.com>
2020-07-28 21:05:55 -0400

    New approach saludo

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-  let name = req.query.name || "desconocido";
+//  let name = req.query.name || "desconocido"; // opcion 1
+  let name = Object.keys(req.query).length ? req.query.name : 'Desconocido'; // opciòn II
   res.send("<h1>Hola " + name + " !</h1>");
 });
 app.listen(3000, () => console.log('Listening on port 3000!'));
